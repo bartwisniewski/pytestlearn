f734b17bbe3ac0c67997182a73b15178e726ec14
bartekwisnia <bartekwisnia@op.pl>
2022-08-30 19:23:23 +0200

    corrected test_fizzbuzz, BDD

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -1,5 +1,5 @@
 from functionality.fizzbuzz import *
-from pytest_bdd import scenario, given, when, then
+from pytest_bdd import scenario, given, when, then, parsers
 
 
 @scenario('fizzbuzz.feature', 'Fizz number')
@@ -22,21 +22,21 @@ def divider_of_3():
     return 6
 
 
-@given("I have a number which is divider of 5 and not of 3")
+@given("I have a number which is divider of 5 and not of 3", target_fixture="number")
 def divider_of_5():
     return 10
 
 
-@given("I have a number which is divider of 3 and of 5")
+@given("I have a number which is divider of 3 and of 5", target_fixture="number")
 def divider_of_3_5():
     return 15
 
 
-@when("when I run fizzbuzz game", target_fixture="fizzbuzz_return")
+@when("I run fizzbuzz game", target_fixture="fizzbuzz_return")
 def fizzbuzz_game(number: int):
     return fizzbuzz(number)
 
 
-@then('Then I should see {expexted} printed')
+@then(parsers.parse('I should see {expected} printed'))
 def test_fizzbuzz_return(expected, fizzbuzz_return):
     assert fizzbuzz_return == expected
